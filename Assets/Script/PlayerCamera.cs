8e7cc793f940e5bf2aea0bbd6493972935ec8cc1
Faiz-A-S <faizasucipto@gmail.com>
2022-07-14 02:15:14 +0700

    Shoot Raycast

--- Assets/Script/PlayerCamera.cs
+++ Assets/Script/PlayerCamera.cs
@@ -15,13 +15,28 @@ public class PlayerCamera : MonoBehaviour
     private float inputVectorY;
     private float inputVectorX;
 
+    public float weaponRange = 50f;
+
+    private Camera fpsCam;
+
     // Start is called before the first frame update
     void Awake()
     {
+        fpsCam = GetComponentInParent<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
         playerInput = new PlayerInput();
         playerInput.Player.Enable();
     }
+
+    void Update()
+    {
+        // Create a vector at the center of our camera's viewport
+        Vector3 lineOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+        // Draw a line in the Scene View  from the point lineOrigin in the direction of fpsCam.transform.forward * weaponRange, using the color green
+        Debug.DrawRay(lineOrigin, fpsCam.transform.forward * weaponRange, Color.green);
+    }
+
     void LateUpdate()
     {       
         inputVector = playerInput.Player.Look.ReadValue<Vector2>();
@@ -34,7 +49,6 @@ public class PlayerCamera : MonoBehaviour
         inputVectorY = Mathf.Clamp(inputVectorY, minC, maxC);
         //transform.localRotation = Quaternion.Euler(inputVectorY, 0f, 0f);
         transform.localEulerAngles = new Vector3(inputVectorY, 0, 0);
-        weapon.localEulerAngles = new Vector3(inputVectorY, 0, 0);
         //player.Rotate(Vector3.up * inputVectorX );
         player.Rotate(new Vector3(0f, (inputVectorX), 0f), Space.Self);
     }
--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -10,20 +10,29 @@ public class PlayerManager : MonoBehaviour
     public Vector2 inputVector;
 
     [Header("Jump")]
-    public float gravity = -9.8f;
+    public float gravity = -30f;
     public Transform spherePosition;
     public LayerMask groundMask;
-    public float sphereRadius = 4f;
+    public float sphereRadius = 0.4f;
     public float jumpHeight = 0.8f;
     public Vector3 velocity;
 
     [Header("Shoot")]
-    public int bulletRate;
-    public float bulletSpeed;
+    public int gunDamage = 1;
+    public float bulletRate = 0.25f;
+    public float bulletSpeed = 1000f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
     public int bulletCount;
     public Rigidbody bullet;
     public Transform muzzle;
+
     private float maxLifeTime = 5f;
+    private Camera fpsCam;  
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
 
     private PlayerInput playerInput;
     private CharacterController charcon;
@@ -35,6 +44,8 @@ public class PlayerManager : MonoBehaviour
     void Awake()
     {
         charcon = GetComponent<CharacterController>();
+        laserLine = GetComponent<LineRenderer>();
+        fpsCam = GetComponentInChildren<Camera>();
 
         playerInput = new PlayerInput();
         playerInput.Player.Enable();
@@ -87,14 +98,66 @@ public class PlayerManager : MonoBehaviour
 
     private void Shoot()
     {
-       playerInput.Player.Shoot.performed += 
-            context =>
+        //playerInput.Player.Shoot.performed += 
+        //     context =>
+        //     {
+        //         Rigidbody bulletClone = Instantiate(bullet, muzzle.position, Quaternion.identity);
+        //         bulletClone.AddForce(transform.forward * bulletSpeed);
+
+        //         Destroy(bulletClone.gameObject, maxLifeTime);
+        //     };
+        if (playerInput.Player.Shoot.IsPressed() && Time.time > nextFire)
+        {
+            Debug.Log("Shoot");
+            nextFire = Time.time + bulletRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+            // Declare a raycast hit to store information about what our raycast has hit
+            RaycastHit hit;
+
+            // Set the start position for our visual effect for our laser to the position of gunEnd
+            laserLine.SetPosition(0, muzzle.position);
+
+            // Check if our raycast has hit anything
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
             {
-                Rigidbody bulletClone = Instantiate(bullet, muzzle.position, Quaternion.identity);
-                bulletClone.AddForce(transform.forward * bulletSpeed);
+                // Set the end position for our laser line 
+                laserLine.SetPosition(1, hit.point);
 
-                Destroy(bulletClone.gameObject, maxLifeTime);
-            };
+                // Get a reference to a health script attached to the collider we hit
+                EnemyHealth health = hit.collider.GetComponent<EnemyHealth>();
+
+                // If there was a health script attached
+                if (health != null)
+                {
+                    // Call the damage function of that script, passing in our gunDamage variable
+                    health.Damage(gunDamage);
+                }
+
+                // Check if the object we hit has a rigidbody attached
+                if (hit.rigidbody != null)
+                {
+                    // Add force to the rigidbody we hit, in the direction from which it was hit
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+            }
+            else
+            {
+                // If we did not hit anything, set the end of the line to a position directly in front of the camera at the distance of weaponRange
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
     }
+    private IEnumerator ShotEffect()
+    {
+        // Turn on our line renderer
+        laserLine.enabled = true;
 
+        //Wait for .07 seconds
+        yield return shotDuration;
+
+        // Deactivate our line renderer after waiting
+        laserLine.enabled = false;
+    }
 }
